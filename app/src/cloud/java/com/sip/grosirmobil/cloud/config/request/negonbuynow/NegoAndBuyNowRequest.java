dd54eeff768545fc05f000153693ce5d83de5ec4
syahrulhajji <syahrulhajji@gmail.com>
2021-01-05 16:59:07 +0700

    Note : 1. Buy Now and Nego

--- app/src/cloud/java/com/sip/grosirmobil/cloud/config/request/negonbuynow/NegoAndBuyNowRequest.java
+++ app/src/cloud/java/com/sip/grosirmobil/cloud/config/request/negonbuynow/NegoAndBuyNowRequest.java
@@ -1,28 +1,28 @@
-package com.sip.grosirmobil.cloud.config.request.buynow;
+package com.sip.grosirmobil.cloud.config.request.negonbuynow;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class BuyNowRequest {
+public class NegoAndBuyNowRequest {
 
-    @SerializedName("open_house_id")
+    @SerializedName("ohid")
     @Expose
-    private String openHouseId;
-
+    private String ohid;
+    
     @SerializedName("kik")
     @Expose
     private String kik;
-
+    
     @SerializedName("agreement_no")
     @Expose
     private String agreementNo;
-
+    
     @SerializedName("bid_price")
     @Expose
     private String bidPrice;
 
-    public BuyNowRequest(String openHouseId, String kik, String agreementNo, String bidPrice) {
-        this.openHouseId = openHouseId;
+    public NegoAndBuyNowRequest(String ohid, String kik, String agreementNo, String bidPrice) {
+        this.ohid = ohid;
         this.kik = kik;
         this.agreementNo = agreementNo;
         this.bidPrice = bidPrice;
